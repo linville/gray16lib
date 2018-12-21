4a6705e2e2e1092c8e5f6e451a08b56d1e8b5e29
Aaron Linville <aaron@linville.org>
2018-12-20 22:21:08 -0500

    Support loading an image via the command line.

--- gray16demo/main.cpp
+++ gray16demo/main.cpp
@@ -1,6 +1,6 @@
 /*!
   \file gray16demo/main.cpp
-  \brief main(..) for the Gray16-Convert Demonstration application.
+  \brief main(..) for the Gray16Demo Demonstration application.
 */
 
 #include "gray16_window.h"
@@ -16,8 +16,32 @@ int main(int argc, char *argv[]) {
   QCoreApplication::setApplicationName("Gray16");
   QCoreApplication::setApplicationVersion(APP_VERSION);
 
+  QCommandLineParser parser;
+  parser.setApplicationDescription("16-bit Grayscale Processing");
+  parser.addHelpOption();
+  parser.addVersionOption();
+  parser.addPositionalArgument("file", "png");
+
+  parser.parse(QApplication::arguments());
+
   Gray16Window mainWindow;
   mainWindow.show();
+
+  const auto args = parser.positionalArguments();
+  if(!args.empty()) {
+    const auto filePath = args.first();
+    if(!QFileInfo::exists(filePath)) {
+      qCritical() << "Can't open" << filePath;
+      return -1;
+    }
+
+    if(!filePath.endsWith(".png", Qt::CaseInsensitive)) {
+      qCritical() << "Doesn't appear to be a PNG.";
+      return -1;
+    }
+
+    mainWindow.openFile(filePath);
+  }
 
   return myApp.exec();
 }
